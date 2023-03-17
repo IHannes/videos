2bdfa8016b06c0fec0cf5e6a9bef61b6e85f590f
IHannes <104006031+IHannes@users.noreply.github.com>
2023-03-17 12:08:04 +0100

    Update vidoessc.sh

--- vidoessc.sh
+++ vidoessc.sh
@@ -3,7 +3,7 @@
 SELECTION=$( zenity --list  --text "Wo möchtest du Videos schauen/downloaden?" --hide-header --column "Select" 'Youtube anschauen' 'Youtube downloaden' 'Soap2Day downloaden' 'Soap2Day anschauen' 'Soap2Day downloaden funktioniert nicht')
 
 if [ "$SELECTION" == "Soap2Day downloaden funktioniert nicht"  ]; then
-        cd /home/frederik/
+        cd ~
         rm -rf soap2day-dl/
         git clone https://github.com/KevCui/soap2day-dl
         cd soap2day-dl/bin/
@@ -28,14 +28,12 @@ case $SELECTION in
                         echo "Du willst $VIDEONAME auf $SELECTION"
                         cd /home/frederik/soap2day-dl
                         terminal +sb -fg green -bg black  -fn "xft:Bitstream Vera Sans:pixelsize=30" -letsp -8 -e  sh soap2day-dl.sh -n "'"$VIDEONAME"'"
-                        #cp /home/frederik/soap2day-dl/\'$VIDEONAME\' home/frederik/Videos/$VIDEONAME
                         ;;
                 "Soap2Day anschauen")
                         VIDEONAME=$(zenity --title="Videoname" --text "Wie heißt das Video?" --entry --width 380)
                         echo "Du willst $VIDEONAME auf $SELECTION"
                         IFS=' ' read -ra array1 <<< "$VIDEONAME"
                         variable2=""
-                        #Print the split string
                         for i in "${array1[@]}"
                         do
                         variable2=$variable2"%20"$i
